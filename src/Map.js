744dde40247a27fc52dabd51e425aaebd22476f7
allanmiller143 <millerallan17@gmail.com>
2024-11-30 16:23:17 -0300

    tentativa de deploy

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine';
 
-const Map = ({ coordinates, setAddress, setFinalAddress, setDialogFields, setDialogFinalFields, setPricePredict,setLoading, }) => {
+const Map = ({ coordinates, setAddress, setFinalAddress, setDialogFields, setDialogFinalFields, setPricePredict, setLoading }) => {
   const mapRef = useRef(null);
-  const routingControlRef = useRef(null); // Ref para persistir o controle de roteamento
-  const startMarkerRef = useRef(null);    // Ref para o marcador de origem
-  const endMarkerRef = useRef(null);      // Ref para o marcador de destino
-
+  const routingControlRef = useRef(null);
+  const startMarkerRef = useRef(null);
+  const endMarkerRef = useRef(null);
 
   const parseAddress = (address) => {
     const addressParts = address.split(', ');
@@ -19,149 +18,11 @@ const Map = ({ coordinates, setAddress, setFinalAddress, setDialogFields, setDia
       state: addressParts[3] || '',
       country: addressParts[4] || '',
     };
-
-    // Exemplo de como exibir o resultado
     return ad;
   };
 
-  useEffect(() => {
-    // Definir o ícone personalizado para os marcadores
-    const defaultIcon = L.icon({
-      iconUrl: require('leaflet/dist/images/marker-icon.png'),
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41],
-    });
-
-    // Inicializar o mapa apenas uma vez
-    const map = L.map(mapRef.current).setView([51.505, -0.09], 5);
-
-    // Adicionar a camada de tiles (OpenStreetMap)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-    }).addTo(map);
-
-    // Inicializar o controle de roteamento
-    const routingControl = L.Routing.control({
-      waypoints: [],
-      routeWhileDragging: true,
-      lineOptions: {
-        styles: [{ color: '#6FA1EC', weight: 4 }],
-      },
-      createMarker: () => null, // Remover marcadores padrão
-      router: L.Routing.osrmv1({ suppressWaypoints: true }),
-      show: false,
-      addWaypoints: false,
-    }).addTo(map);
-
-    routingControlRef.current = routingControl; // Armazenar o routingControl no ref
-
-    // Remover o painel de roteamento manualmente
-    const pane = document.querySelector('.leaflet-routing-container');
-    if (pane) pane.style.display = 'none';
-
-    // Evento de clique no mapa
-    map.on('click', async (e) => {
-      const { lat, lng } = e.latlng;
-      if (!startMarkerRef.current) {
-        startMarkerRef.current = L.marker([lat, lng], { icon: defaultIcon, draggable: true })
-          .addTo(map)
-          .bindPopup('Origem')
-          .openPopup();
-        startMarkerRef.current.on('dragend', updateRoute);
-        
-        // Pegar o endereço ao clicar no mapa
-        const address = await getAddress(lat, lng);
-        console.log(`Origem: ${address}`);
-        setAddress(address);
-
-        const addressParts = parseAddress(address);
-        setDialogFields(addressParts);
-        
-  
-        
-      } else if (!endMarkerRef.current) {
-        endMarkerRef.current = L.marker([lat, lng], { icon: defaultIcon, draggable: true })
-          .addTo(map)
-          .bindPopup('Destino')
-          .openPopup();
-        endMarkerRef.current.on('dragend', updateRoute);
-        
-        // Pegar o endereço ao clicar no mapa
-        const address = await getAddress(lat, lng);
-        console.log(`Destino: ${address}`);
-        setFinalAddress(address);
-        const addressParts = parseAddress(address);
-        setDialogFinalFields(addressParts);
-        
-        updateRoute();
-      }
-    });
-
-    // Resetar os marcadores ao clicar com o botão direito
-    map.on('contextmenu', () => {
-      if (startMarkerRef.current) {
-        map.removeLayer(startMarkerRef.current);
-        startMarkerRef.current = null;
-        setAddress('');
-        setDialogFields({
-          street: '',
-          city: '',
-          state: '',
-          neighborhood: '',
-          houseNumber: '',
-          country: '',
-        });
-      }
-      if (endMarkerRef.current) {
-        map.removeLayer(endMarkerRef.current);
-        endMarkerRef.current = null;
-        setFinalAddress('');
-        setDialogFinalFields({
-          street: '',
-          city: '',
-          state: '',
-          neighborhood: '',
-          houseNumber: '',
-          country: '',
-        });
-      }
-      setPricePredict(null);
-      routingControl.setWaypoints([]);
-    });
-
-    // Cleanup do mapa
-    return () => map.remove();
-  }, [ setAddress, setFinalAddress, setDialogFields, setDialogFinalFields ]);
-
-  useEffect(() => {
-    if (coordinates.origin && coordinates.destination) {
-      const { origin, destination } = coordinates;
-
-      // Verificar se o controle de roteamento e os marcadores existem
-      if (routingControlRef.current) {
-        routingControlRef.current.setWaypoints([
-          L.latLng(origin.lat, origin.lng),
-          L.latLng(destination.lat, destination.lng),
-        ]);
-      }
-
-      if (startMarkerRef.current) {
-        startMarkerRef.current.setLatLng([origin.lat, origin.lng]);
-        startMarkerRef.current.bindPopup('Origem');
-      }
-
-      if (endMarkerRef.current) {
-        endMarkerRef.current.setLatLng([destination.lat, destination.lng]);
-        endMarkerRef.current.bindPopup('Destino');
-      }
-    }
-  }, [coordinates]);
-
-  // Função para atualizar a rota quando os marcadores são arrastados
-  const updateRoute = () => {
+  // Memoizing the updateRoute function
+  const updateRoute = useCallback(() => {
     if (startMarkerRef.current && endMarkerRef.current) {
       const waypoints = [startMarkerRef.current.getLatLng(), endMarkerRef.current.getLatLng()];
       routingControlRef.current.setWaypoints(waypoints);
@@ -178,45 +39,139 @@ const Map = ({ coordinates, setAddress, setFinalAddress, setDialogFields, setDia
         setPricePredict(data.predictedFare);
       }).catch((err) => {
         console.error('Erro:', err);
-     });
+      });
     }
-  };
+  }, [setPricePredict]); // Dependencies: only needs setPricePredict to trigger updateRoute.
 
-  async function postToServer(data) {
+  useEffect(() => {
+    const defaultIcon = L.icon({
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
+
+    const map = L.map(mapRef.current).setView([51.505, -0.09], 5);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(map);
+
+    const routingControl = L.Routing.control({
+      waypoints: [],
+      routeWhileDragging: true,
+      lineOptions: { styles: [{ color: '#6FA1EC', weight: 4 }] },
+      createMarker: () => null,
+      router: L.Routing.osrmv1({ suppressWaypoints: true }),
+      show: false,
+      addWaypoints: false,
+    }).addTo(map);
+
+    routingControlRef.current = routingControl;
+
+    const pane = document.querySelector('.leaflet-routing-container');
+    if (pane) pane.style.display = 'none';
+
+    map.on('click', async (e) => {
+      const { lat, lng } = e.latlng;
+      if (!startMarkerRef.current) {
+        startMarkerRef.current = L.marker([lat, lng], { icon: defaultIcon, draggable: true })
+          .addTo(map)
+          .bindPopup('Origem')
+          .openPopup();
+        startMarkerRef.current.on('dragend', updateRoute);
+
+        const address = await getAddress(lat, lng);
+        setAddress(address);
+        setDialogFields(parseAddress(address));
+      } else if (!endMarkerRef.current) {
+        endMarkerRef.current = L.marker([lat, lng], { icon: defaultIcon, draggable: true })
+          .addTo(map)
+          .bindPopup('Destino')
+          .openPopup();
+        endMarkerRef.current.on('dragend', updateRoute);
+
+        const address = await getAddress(lat, lng);
+        setFinalAddress(address);
+        setDialogFinalFields(parseAddress(address));
+
+        updateRoute(); // Update the route immediately after setting destination
+      }
+    });
+
+    map.on('contextmenu', () => {
+      if (startMarkerRef.current) {
+        map.removeLayer(startMarkerRef.current);
+        startMarkerRef.current = null;
+        setAddress('');
+        setDialogFields({
+          street: '', city: '', state: '', neighborhood: '', houseNumber: '', country: '',
+        });
+      }
+      if (endMarkerRef.current) {
+        map.removeLayer(endMarkerRef.current);
+        endMarkerRef.current = null;
+        setFinalAddress('');
+        setDialogFinalFields({
+          street: '', city: '', state: '', neighborhood: '', houseNumber: '', country: '',
+        });
+      }
+      setPricePredict(null);
+      routingControl.setWaypoints([]);
+    });
+
+    return () => map.remove();
+  }, [setAddress, setFinalAddress, setDialogFields, setDialogFinalFields, setPricePredict, updateRoute]);
+
+  useEffect(() => {
+    if (coordinates.origin && coordinates.destination) {
+      const { origin, destination } = coordinates;
+      if (routingControlRef.current) {
+        routingControlRef.current.setWaypoints([
+          L.latLng(origin.lat, origin.lng),
+          L.latLng(destination.lat, destination.lng),
+        ]);
+      }
+
+      if (startMarkerRef.current) {
+        startMarkerRef.current.setLatLng([origin.lat, origin.lng]).bindPopup('Origem');
+      }
+      if (endMarkerRef.current) {
+        endMarkerRef.current.setLatLng([destination.lat, destination.lng]).bindPopup('Destino');
+      }
+      updateRoute();
+    }
+  }, [coordinates, updateRoute]);
+
+  const postToServer = async (data) => {
     const url = 'http://15.228.36.187:3000';
     setLoading(true);
     setPricePredict(null);
     try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json' // Ajuste conforme necessário
-            },
-            body: JSON.stringify(data) // Converte o objeto para JSON
-        });
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
 
-        if (!response.ok) {
-            throw new Error(`Erro HTTP: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        console.log('Resposta do servidor:', responseData);
-        return responseData; // Retorna a resposta
+      if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+      const responseData = await response.json();
+      return responseData;
     } catch (error) {
-        console.error('Erro ao fazer o POST:', error.message);
-        throw error;
-    }finally{
+      console.error('Erro ao fazer o POST:', error.message);
+      throw error;
+    } finally {
       setLoading(false);
     }
-}
+  };
 
-  // Função para obter o endereço com base na latitude e longitude usando o Nominatim
   const getAddress = async (lat, lng) => {
     const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`;
     try {
       const response = await fetch(url);
       const data = await response.json();
-      return data.display_name; // Retorna o endereço completo
+      return data.display_name;
     } catch (error) {
       console.error("Erro ao buscar endereço:", error);
       return "Endereço não encontrado";
